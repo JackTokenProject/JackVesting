39f8bb8cb65accdd17227d102b66baaa6209dbf1
veis sadikov <veissadikov@veiss-MacBook-Pro.local>
2024-10-31 11:54:47 +0400

    Issue QA report #6 QA-02 fix

--- contracts/JackToken.sol
+++ contracts/JackToken.sol
@@ -1,8 +1,12 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
-import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
+import {ERC20Burnable} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+
+
+
 
 contract JACK is ERC20Burnable, Ownable {
     uint256 public maxSupply = 1_000_000_000 * (10 ** decimals());
